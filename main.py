02e84a506e476f704b0c327eb8cbcdf0ed9521ab
bunyamin-polat <muhendisbp@gmail.com>
2026-07-27 23:55:29 +0200

    refactor main.py for improved structure and async handling

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
-from dotenv import load_dotenv
 import asyncio
-load_dotenv()
+import os
 
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_agent
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
-from langchain_openai import ChatOpenAI
-from langchain_mcp_adapters.tools import load_mcp_tools
-from langchain.agents import create_agent
 
+load_dotenv()
 
-llm = ChatOpenAI()
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
 stdio_server_params = StdioServerParameters(
     command="python",
-    args=["/Users/bunyamin/Desktop/AI_Course/MCP/servers/math_server.py"]
+    args=["./servers/math_server.py"],
 )
 
 async def main():
-    print("Hello from MCP!")
+    async with stdio_client(stdio_server_params) as (read,write):    
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("Session initialized!...")
+            tools = await load_mcp_tools(session)
 
+            agent = create_agent(llm,tools)
+
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 54 + 2 * 3?")]})
+            print(result["messages"][-1].content)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
