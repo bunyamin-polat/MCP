eb882a58435eae176e9e9a1df83d8408de49e159
bunyamin-polat <muhendisbp@gmail.com>
2026-07-27 23:45:21 +0200

    refactor main.py to integrate async functionality and enhance server parameters

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from dotenv import load_dotenv
+import asyncio
 load_dotenv()
 
-def main():
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain.agents import create_agent
+
+
+llm = ChatOpenAI()
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/Users/bunyamin/Desktop/AI_Course/MCP/servers/math_server.py"]
+)
+
+async def main():
     print("Hello from MCP!")
 
 
